f7b499655e03d41cfd53745b9e01c1afdb6b683d
정윤환 <109188479+Torychu@users.noreply.github.com>
2023-04-11 17:32:35 +0900

    Add files via upload

---
+++ School/221020.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+//	int score[5];
+//	int i;
+//	
+//	score[0] = 10;
+//	scanf("%d", &score[1]);
+//	score[2] = score[0] + score[1];
+//	score[3] = abs(score[0]) - abs(score[1]);
+//	score[4] = score[0] * score[1];
+//	
+//	for(i = 0; i < 5; i++)
+//		printf("%d ",score[i]);
+
+//	int i;
+//	int num[3];
+//	
+//	num[0] = 10;
+//	num[1] = 20;
+//	num[2] = 30;
+//	
+//	for(i = 0; i < 3; i++)
+//		printf("num[%d] = %d\n", i, num[i]);
+//
+//	printf("int ũ�� : %d\n", sizeof(int));
+//	printf("i ũ�� : %d\n", sizeof(i));
+
+//	int arr[5] = {10, 15, 85, 66, 20};
+//	printf("%d\n",arr[0]);
+//	printf("%d\n",arr[2]);
+//	printf("%d\n",arr[4]);
+
+//	int a[10], i;
+//	
+//	for(i = 0; i < 10; i++)
+//		a[i] = 5;
+//	printf("a[5]: %d\n", a[5]);
+//	printf("a[7]: %d\n", a[7]);
+	
+//	int data[6], i;
+//	
+//	for(i = 0; i < 6; i++)
+//	{
+//		printf("������ �Է� : ");
+//		scanf("%d", &data[i]);
+//	}
+//	for (i = 5; i >= 0; i--)
+//		printf("%d ",data[i]);
+}
